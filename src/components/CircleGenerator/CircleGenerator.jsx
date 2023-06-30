6500cb1daee6fc23be4f9010d7175777e36aedbd
ManuCasanueva <manucasanueva3@gmail.com>
2023-06-30 12:32:21 -0300

    Circles Fixed

--- src/components/CircleGenerator/CircleGenerator.jsx
+++ src/components/CircleGenerator/CircleGenerator.jsx
@@ -3,26 +3,80 @@ import { TextField, Button } from "@mui/material";
 import './CircleGenerator.css';
 
 const CircleGenerator = () => {
-  const [width, setWidth] = useState(20);
-  const [height, setHeight] = useState(20);
+  const [width, setWidth] = useState(25);
+  const [height, setHeight] = useState(25);
   const [pixels, setPixels] = useState([]);
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(true);
   const [centerX, setCenterX] = useState(0);
   const [centerY, setCenterY] = useState(0);
 
+
   const generatePixels = () => {
-    if (width === height) {
-      generateCirclePixels();
+    if (width === 0 || height === 0) {
+      setPixels([]);
+      return;
     }
   
-    setCenterX(width % 2 === 0 ? (width - 1) / 2 : Math.floor(width / 2));
-    setCenterY(height % 2 === 0 ? (height - 1) / 2 : Math.floor(height / 2));
+    if (width === height) {
+      generateCirclePixels();
+    } else {
+      setCenterX(width % 2 === 0 ? (width - 1) / 2 : Math.floor(width / 2));
+      setCenterY(height % 2 === 0 ? (height - 1) / 2 : Math.floor(height / 2));
+      const newPixels = [];
+  
+      for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+          if (
+            x >= centerX - Math.floor(width / 2) &&
+            x <= centerX + Math.floor((width - 1) / 2) &&
+            y >= centerY - Math.floor(height / 2) &&
+            y <= centerY + Math.floor((height - 1) / 2)
+          ) {
+            newPixels.push({ x, y, filled: true, isCenter: true });
+          } else if (
+            (x === centerX && y >= centerY - Math.floor(height / 2) && y <= centerY + Math.floor((height - 1) / 2)) ||
+            (y === centerY && x >= centerX - Math.floor(width / 2) && x <= centerX + Math.floor((width - 1) / 2))
+          ) {
+            newPixels.push({ x, y, filled: true });
+          } else {
+            newPixels.push({ x, y, filled: false });
+          }
+        }
+      }
+  
+      if (width % 2 !== 0 && height % 2 !== 0) {
+        // Agregar líneas rectas desde el centro hasta cada borde
+        const left = Math.floor((width - 1) / 2);
+        const right = Math.floor(width / 2);
+        const top = Math.floor((height - 1) / 2);
+        const bottom = Math.floor(height / 2);
+  
+        for (let x = centerX - left; x <= centerX + right; x++) {
+          newPixels.push({ x, y: centerY, filled: true });
+        }
+  
+        for (let y = centerY - top; y <= centerY + bottom; y++) {
+          newPixels.push({ x: centerX, y, filled: true });
+        }
+      }
+  
+      setPixels(newPixels);
+    }
   };
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
   const generateCirclePixels = () => {
-    const radius = Math.floor(Math.min(width, height) / 2 - 0.5);
-    const centerX = width / 2 - 0.5;
-    const centerY = height / 2 - 0.5;
+    const radius = Math.floor(Math.min(width, height) / 2);
+    const centerX = width % 2 === 0 ? (width - 1) / 2 : Math.floor(width / 2);
+    const centerY = height % 2 === 0 ? (height - 1) / 2 : Math.floor(height / 2);
     const newPixels = [];
   
     for (let y = 0; y < height; y++) {
@@ -33,7 +87,7 @@ const CircleGenerator = () => {
   
         if (Math.abs(distanceToCenter - radius) <= 0.5) {
           if (x === centerX && y === centerY) {
-            newPixels.push({ x, y, filled: true, isCenter: true });
+            newPixels.push({ x, y, filled: true, });
           } else {
             // Check if the current pixel is part of the line in the X-axis or Y-axis
             if (x === centerX || y === centerY) {
@@ -48,8 +102,16 @@ const CircleGenerator = () => {
       }
     }
   
+    if (width % 2 !== 0 && height % 2 !== 0) {
+      newPixels.push({ x: centerX, y: centerY, filled: true, isCenter: true });
+    }
+  
     setPixels(newPixels);
   };
+  
+
+  
+  
   
   
 
@@ -80,7 +142,7 @@ const CircleGenerator = () => {
         const inCenterY = pixel.y >= centerY - 1 && pixel.y <= centerY + 1;
 
         if (inCenterX && inCenterY) {
-          return { ...pixel, isCenter: true };
+          return { ...pixel,  };
         } else {
           return pixel;
         }
